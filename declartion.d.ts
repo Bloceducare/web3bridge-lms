4d4e09761751c55b9fa2695418ab4c8e144ada5d
Amiola Demilade <amiolademilade@gmail.com>
2025-03-26 10:00:11 +0100

    courses

--- declartion.d.ts
+++ declartion.d.ts
@@ -1,5 +1,20 @@
-declare module "*.svg" {
-    const content: React.FC<React.SVGProps<SVGSVGElement>>;
-    export default content;
-  }
-  
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+// declare module "*.svg" {
+//   const value: string;
+//   export default value;
+// }
+
+declare module '*.svg' {
+  import { FC, SVGProps } from 'react'
+  const content: FC<SVGProps<SVGElement>>
+  export default content
+}
+
+declare module '*.svg?url' {
+  const content: any
+  export default content
+}
